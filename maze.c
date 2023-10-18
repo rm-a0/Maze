eed7e487d950395faed9d73ed58bea51438e10fa
rm-a0 <148249444+rm-a0@users.noreply.github.com>
2023-10-18 09:23:36 +0100

    Add files via upload

--- maze.c
+++ maze.c
@@ -5,7 +5,9 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <unistd.h>
 #define MAX_LENGTH 100
+#define ASCII_ZERO 48
 
 //struct containing matrix
 typedef struct {
@@ -14,8 +16,33 @@ typedef struct {
     unsigned char *cells;
 } Map;
 
+//function that reads data from the file
+void scanFile(int *rows, int *cols)
+{
+    char line[MAX_LENGTH];
+
+    //if there is no file specified, exit function and display error
+    if (isatty(STDIN_FILENO))
+    {
+	    fprintf(stderr, "No file specified \n");
+	    exit(1);
+    }
+
+    if (fgets(line, MAX_LENGTH, stdin) != NULL)                         //TTTTTTOOOOOOODDDDDDOOOOO wrong input 
+    {
+        //convert values to integers
+        *rows = line[0] - ASCII_ZERO;                                   //doesnt work for multiple numbers, FIX
+        *cols = line[2] - ASCII_ZERO;
+    }
+    else
+    {
+        fprintf(stderr, "Failed to read data from the file");
+        exit(1);
+    }
+}
+
 //funnction that allocates memory for 2d matrix and returns pointer to a Map struct
-Map* createMap(int r, int c) 
+Map* createMap(int rows, int cols) 
 {
     //allocates memory for Map struct
     Map *map = (Map *)malloc(sizeof(Map));
@@ -26,11 +53,11 @@ Map* createMap(int r, int c)
         return NULL;
     }
 
-    map->rows = r;
-    map->cols = c;
+    map->rows = rows;
+    map->cols = cols;
 
     //allocates memory for 2d matrix cells
-    map->cells = (unsigned char *)malloc(r * c * sizeof(unsigned char));
+    map->cells = (unsigned char *)malloc(rows * cols * sizeof(unsigned char));
     if (map->cells == NULL) 
     {
         fprintf(stderr, "Memory allocation for the matrix failed\n");
@@ -39,8 +66,15 @@ Map* createMap(int r, int c)
         free(map);
         return NULL;
     }
-
+    //return pointer to a struct called Map
     return map;
+}
+
+//function that gets rid of all allocated memory
+void freeMap(Map *map)
+{
+    free(map->cells);
+    free(map);
 }
 
 //function that appends data to matrix from stdin
@@ -50,24 +84,25 @@ void appendToMap(Map *map)
 }
 
 //function that checks if the content of the file is valid
-int test (void) 
+void testMap(Map *map) 
 {
     printf("TODO 2\n");
 }
 
 int main(int agrc, char *argv[])
 {
-    const char *str1 = "--help";
-    const char *str2 = "--test";
-    const char *str3 = "--rpath";
-    const char *str4 = "--lpath";
-    const char *str5 = "--shortest";
+    const char *help = "--help";
+    const char *test = "--test";
+    const char *rpath = "--rpath";
+    const char *lpath = "--lpath";
+    const char *shortest = "--shortest";
 
     int rows;
     int cols; 
 
     //display instructions when '--help' is the user input
-    if (strcmp(argv[1], str1) == 0) {
+    if (strcmp(argv[1], help) == 0) 
+    {
         printf("Input a file containing matrix of numbers\n");
         printf("Type '--test < file' to check if data in the matrix are valid\n");
         printf("Type '--rpath R C < file' to solve labyrinth using the right hand rule\n");
@@ -75,13 +110,17 @@ int main(int agrc, char *argv[])
         printf("Type '--shortest R C < file' to find the shortest path in the labyrinth\n");
         printf("*R C = row and collumn you want to start on*\n");
     }
-    else if (strcmp(argv[1], str2) == 0) {
+    else if (strcmp(argv[1], test) == 0) 
+    {
+        //read from file to get the necessary data
+        scanFile(&rows, &cols);
+        printf("Rows: %d Cols: %d\n", rows, cols);
         //create a map and fill it with data drom a file
         Map *map = createMap(rows, cols);
         appendToMap(map);
 
         //run test to check if data in the matrix are correct
-        test();
+        testMap(map);
 
         //free allocated memory
         freeMap(map);
